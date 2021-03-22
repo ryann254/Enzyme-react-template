1d9397b5b7c8bcf9646996509d07a462033e33ab
Ryan Njoroge <keddelyronjoz@gmail.com>
2021-03-22 20:54:33 +0300

    feat: Added redux

---
+++ src/createStore.js
@@ -0,0 +1,9 @@
+import {applyMiddleware, createStore} from 'redux'
+import ReduxThunk from 'redux-thunk'
+import rootReducer from './reducers'
+
+export const middlewares = [ReduxThunk]
+
+export const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore)
+
+export const store = createStoreWithMiddleware(rootReducer)
